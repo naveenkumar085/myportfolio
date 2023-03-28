c7d0cd062cc7f86344f04a6d7969f9875cfd2942
Naveen kumar <128359828+naveenkumar085@users.noreply.github.com>
2023-03-28 21:51:26 +0530

    Update contect.jsx

--- src/Components/Contect/contect.jsx
+++ src/Components/Contect/contect.jsx
@@ -7,7 +7,7 @@ const contect = () => {
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
-      <h2>Contect Me</h2>
+      <h2>Contact Me</h2>
       <div className="container contact__container">
         <div className="contact__option">
         
